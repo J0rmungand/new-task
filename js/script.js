cfd97dbd36f0d69be650de54cc40683d859b022c
SosnovskyGleb <jormungand1993@yandex,ru>
2018-11-27 19:34:25 +0300

    Gallery v2.2

--- js/script.js
+++ js/script.js
@@ -1,119 +1,172 @@
+'use strict';
 
-var currentAlbum = 1;
+function $(elem) {
+	return document.querySelector(elem);
+}
 
-document.querySelector('.albumNext').addEventListener('click', getNextAlbum);
-document.querySelector('.albumPrev').addEventListener('click', getPrevAlbum);
-
-function createGalleryElement (element,nameClass) {
-	if(typeof nameClass == "undefined") {
-		return document.createElement(element);
-	} else {
-		element = document.createElement(element);
-		element.className=nameClass;
-		return element;	
+function $extendObj(_def, addons) {
+	if (typeof addons !== "undefined") {
+		for (var prop in _def) {
+			if (addons[prop] != undefined) {
+				_def[prop] = addons[prop];
+			}
+		}
 	}
 }
 
-function append (parent, element) {
+
+function createGalleryElement(element, nameClass) {
+	if (typeof nameClass == "undefined") {
+		return document.createElement(element);
+	} else {
+		element = document.createElement(element);
+		element.className = nameClass;
+		return element;
+	}
+}
+
+function append(parent, element) {
 	return parent.appendChild(element);
 }
 
-function clearContentBySelector (element) {
+function clearContentBySelector(element) {
 	return document.querySelector(element).innerHTML = '';
 }
 
-function openModal () {
-    var overlay = document.querySelector('.product-image-overlay');
-    return overlay.style.display='block';
+function openModal() {
+	var overlay = $('.product-image-overlay');
+	return overlay.style.display = 'block';
 }
 
-function closeModal () {
-	var overlay = document.querySelector('.product-image-overlay');
-    return overlay.style.display='none';
+function closeModal() {
+	var overlay = $('.product-image-overlay');
+	return overlay.style.display = 'none';
 }
 
-function getSrc () {
-	large = document.querySelector('.large');
+function getSrc() {
+	var large = $('.large');
 	large.src = this.dataset.largeSrc;
 }
 
-function clickIn (element, func) {
+function clickIn(element, func) {
 	return element.addEventListener('click', func);
 }
 
-function getAlbum () {
-	fetch('https://jsonplaceholder.typicode.com/albums/' + currentAlbum + '/photos')
-	.then(response => response.json())
-	.then(function (data) {
-		var photos = data;
-		
-		return photos.forEach(function (photo) {
-			var div = createGalleryElement('div', 'albumItem'),
-				a = createGalleryElement('a'),
-				large = document.querySelector('.large'),
-				close = document.querySelector('.product-image-overlay-close'),
-				img = createGalleryElement('img');
-				
+var gallery_plugin = (function () {
 
+	var getAlbum = function (settings) {
+		var _ = this;
+		_.def = {
+			target: $('.albumBody'),
+			arrowLeft: $('.albumPrev'),
+			arrowRight: $('.albumNext'),
+			currentAlbum: '1'
+		}
 
-			a.className = 'small';
-			a.href = '#';
-			a.dataset.largeSrc = photo.url;
-			img.src = photo.thumbnailUrl;
-			img.alt = photo.title;
+		$extendObj(_.def, settings);
+		_.init();
+	}
+	getAlbum.prototype.init = function () {
+		var _ = this;
+		$('.spinner').style.display = "block";
+			_.def.target.innerHTML = "";
+			setTimeout(function () {
+				_.album();
+				$('.spinner').style.display = "none";
+			}, 1000);
+		_.initArrows();
+	}
+	getAlbum.prototype.album = function () {
+		var _ = this;
+		_.def.target.innerHTML = "";
+		fetch('https://jsonplaceholder.typicode.com/albums/' + _.def.currentAlbum + '/photos')
+			.then(response => response.json())
+			.then(function (data) {
+				var photos = data;
 
-			clickIn(a, openModal);
-			clickIn(a, getSrc);
-			clickIn(close, closeModal);
-
-			append(document.querySelector('.albumBody'), div);
-			append(div, a);
-			append(a, img);
-			
-		})
-	})
-
-	fetch('https://jsonplaceholder.typicode.com/albums/' + currentAlbum)
-	.then(response => response.json())
-	.then(function (album) {
-		var span = createGalleryElement('span'),
-			div = createGalleryElement('div', 'albumTtile');
-
-		span.innerHTML = album.title;
-
-		append(document.querySelector('.albumBody'), div);
-		append(div, span);
-	})
-
-	.catch (function (error) {
-		console.log(error);
-	});
-}
-
-function getNextAlbum () {
-	currentAlbum++;
-	if (currentAlbum > 100) {
-		currentAlbum = 1;
-	} 
-	clearContentBySelector('.albumBody');
-	getAlbum();
-}
-
-
-function getPrevAlbum () {
-	currentAlbum--;
-	if (currentAlbum < 1) {
-		currentAlbum = 100;
-	} 
-	clearContentBySelector('.albumBody');
-	getAlbum();
-}
-
-getAlbum();
+				return photos.forEach(function (photo) {
+					var div = createGalleryElement('div', 'albumItem'),
+						a = createGalleryElement('a'),
+						large = $('.large'),
+						close = $('.product-image-overlay-close'),
+						img = createGalleryElement('img');
 
 
 
+					a.className = 'small';
+					a.href = '#';
+					a.dataset.largeSrc = photo.url;
+					img.src = photo.thumbnailUrl;
+					img.alt = photo.title;
+
+					clickIn(a, openModal);
+					clickIn(a, getSrc);
+					clickIn(close, closeModal);
+
+					append(_.def.target, div);
+					append(div, a);
+					append(a, img);
+
+				})
+			})
+
+		fetch('https://jsonplaceholder.typicode.com/albums/' + _.def.currentAlbum)
+			.then(response => response.json())
+			.then(function (album) {
+				var span = createGalleryElement('span'),
+					div = createGalleryElement('div', 'albumTtile');
+
+				span.innerHTML = album.title;
+
+				append(_.def.target, div);
+				append(div, span);
+			})
+
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	getAlbum.prototype.initArrows = function () {
+		var _ = this;
+		_.def.arrowRight.addEventListener('click', function () {
+			_.def.currentAlbum++;
+
+			if (_.def.currentAlbum > 100) {
+				_.def.currentAlbum = 1;
+			}
+			$('.spinner').style.display = "block";
+			_.def.target.innerHTML = "";
+			setTimeout(function () {
+				_.album();
+				$('.spinner').style.display = "none";
+			}, 1000);
+		}, false);
+
+		_.def.arrowLeft.addEventListener('click', function () {
+			_.def.currentAlbum--;
+
+			if (_.def.currentAlbum < 1) {
+				_.def.currentAlbum = 100;
+			}
+			$('.spinner').style.display = "block";
+			_.def.target.innerHTML = "";
+			setTimeout(function () {
+				_.album();
+				$('.spinner').style.display = "none";
+			}, 1000);
+		}, false);
+	}
 
 
+	return getAlbum;
+})();
 
+//Инициализация альбома 
 
+var gallery = new gallery_plugin({
+	target: $('.albumBody'),
+	arrowLeft: $('.albumPrev'),
+	arrowRight: $('.albumNext'),
+	currentAlbum: '2'
+});
